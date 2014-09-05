jemalloc/jemalloc

Refactor chunk map.
Break the chunk map into two separate arrays, in order to improve cache
locality. This is related to issue #23.

include/jemalloc/internal/chunk.h
@@ -41,6 +41,7 @@ extern size_t		chunksize;
 extern size_t		chunksize_mask; /* (chunksize - 1). */
 extern size_t		chunk_npages;
 extern size_t		map_bias; /* Number of arena chunk header pages. */
+extern size_t		link_offset;
 extern size_t		arena_maxclass; /* Max size class for arenas. */
 
 void	*chunk_alloc_base(size_t size);

src/chunk.c
@@ -28,6 +28,7 @@ size_t		chunksize;
 size_t		chunksize_mask; /* (chunksize - 1). */
 size_t		chunk_npages;
 size_t		map_bias;
+size_t		link_offset;
 size_t		arena_maxclass; /* Max size class for arenas. */
 
 /******************************************************************************/

MotorolaMobilityLLC/external-jemalloc

include/jemalloc/internal/chunk.h
@@ -41,6 +41,7 @@ extern size_t		chunksize;
 extern size_t		chunksize_mask; /* (chunksize - 1). */
 extern size_t		chunk_npages;
 extern size_t		map_bias; /* Number of arena chunk header pages. */
+extern size_t		map_misc_offset;
 extern size_t		arena_maxclass; /* Max size class for arenas. */
 
 void	*chunk_alloc_base(size_t size);

src/chunk.c
@@ -28,6 +28,7 @@ size_t		chunksize;
 size_t		chunksize_mask; /* (chunksize - 1). */
 size_t		chunk_npages;
 size_t		map_bias;
+size_t		map_misc_offset;
 size_t		arena_maxclass; /* Max size class for arenas. */
 
 /******************************************************************************/